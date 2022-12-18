e084fc5b7d98a0c27603f5158d59dd7a757e2296
LOHHAMILTON <tttggg963@naver.com>
2022-12-18 13:23:45 +0900

    2920 음계

--- BaekJoon/BaekJoon/Program.cs
+++ BaekJoon/BaekJoon/Program.cs
@@ -1,15 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BaekJoon
-{ 
-    internal class Program
+internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var input = Console.ReadLine().Split(' ');
+        int[] input01 = new int[input.Length];
+        bool isAscending = false;
+        bool isDescending = false;
+
+        for (int i = 0; i < input.Length; i++)
         {
+            input01[i] = int.Parse(input[i]);
         }
+
+        for (int i = 1; i < input01.Length; i++)
+        {
+            if (input01[i] > input01[i - 1])
+            {
+                isAscending = true;
+            }
+            else
+            {
+                isAscending = false;
+                break;
+            }
+        }
+        for (int i = 1; i < input01.Length; i++)
+        {
+            if (input01[i] < input01[i - 1])
+            {
+                isDescending = true;
+            }
+            else
+            {
+                isDescending = false;
+                break;
+            }
+        }
+
+        if(isAscending ) 
+        {
+            Console.WriteLine("ascending");
+        }
+        else if(isDescending)
+        {
+            Console.WriteLine("descending");
+        }
+        else if(isAscending == false && isDescending == false)
+        {
+            Console.WriteLine("mixed");
+
+        }
+
     }
 }
