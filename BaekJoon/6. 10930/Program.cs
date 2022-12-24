6fb1c0f90a8898d2e06969c7b886b9f2d5add081
LOHHAMILTON <tttggg963@naver.com>
2022-12-24 14:17:30 +0900

    6. 10930

---
+++ BaekJoon/6. 10930/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System;
+
+internal class program
+{
+    static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        SHA256 sha = new SHA256Managed();
+        byte[] hash = sha.ComputeHash(Encoding.UTF8.GetBytes(sr.ReadLine()));
+        StringBuilder stringBuilder = new();
+        foreach (byte b in hash)
+        {
+            stringBuilder.AppendFormat("{0:x2}", b);
+        }
+        Console.WriteLine(stringBuilder.ToString());
+    }
+}
