7dfcf39e56678072a07e0f8393b49c8db539a02e
LOHHAMILTON <tttggg963@naver.com>
2022-12-24 15:15:25 +0900

    7. 1920

---
+++ BaekJoon/7. 1920/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main( )
+    {
+        StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+        StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
+        StringBuilder sb = new();
+        sr.ReadLine();
+        int[] inputA = Array.ConvertAll(sr.ReadLine().Split(" "), int.Parse);
+
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for(int i = 0; i < inputA.Length; i++)
+        {
+            map[inputA[i]] = i;
+        }
+        sr.ReadLine();
+        int[] inputB = Array.ConvertAll(sr.ReadLine().Split(" "), int.Parse);
+        for(int i = 0; i < inputB.Length; i++)
+        {
+            if (map.ContainsKey(inputB[i]))
+            {
+                sb.AppendLine("1");
+            }
+            else sb.AppendLine("0");
+        }
+        sw.WriteLine(sb);
+        sw.Close();
+    }
+}
