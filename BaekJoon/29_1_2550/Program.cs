57899d97b807330fe63a893118afa633190ce225
LOHHAMILTON <tttggg963@naver.com>
2023-01-08 16:57:55 +0900

    2023_01_18_02

---
+++ BaekJoon/29_1_2550/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class Program
+{
+    static Node[] nodes;
+    static int[] visit;
+    static int[] min, max;
+    class Node
+    {
+        public int Item { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(int item, Node left = null, Node right = null)
+        {
+            Item = item;
+            Left = left;
+            Right = right;
+        }
+    }
+
+    static Node GetNode(int idx)
+    {
+        if (idx == -1) return null;
+        if (nodes[idx] == null)
+        {
+            nodes[idx] = new Node(idx);
+        }
+
+        return nodes[idx];
+    }
+
+    static int DFS(Node node)
+    {
+        ref int ret = ref visit[node.Item];
+        if (ret != 0) return ret;
+
+        ret = 1;
+        if (node.Right != null) ret += DFS(node.Right);
+
+        if (node.Left != null) ret += DFS(node.Left);
+
+        return ret;
+    }
+
+    static void f(int rootIndx, int startWidth, int height)
+    {
+        var leftNode = GetNode(rootIndx).Left;
+        var rightNode = GetNode(rootIndx).Right;
+
+        int leftWidth, rightWidth;
+
+        int middle;
+        if (leftNode != null)
+        {
+            middle = startWidth + visit[leftNode.Item] + 1;
+        }
+        else
+        {
+            middle = startWidth + 1;
+        }
+
+        if (leftNode == null)
+        {
+            leftWidth = -1;
+        }
+        else
+        {
+            if (leftNode.Left == null) leftWidth = startWidth + 1;
+            else
+            {
+                leftWidth = startWidth + visit[leftNode.Left.Item] + 1;
+            }
+
+            f(leftNode.Item, startWidth, height + 1);
+        }
+
+        if (rightNode == null)
+        {
+            rightWidth = -1;
+        }
+        else
+        {
+            if (rightNode.Left == null) rightWidth = middle + 1;
+            else rightWidth = middle + visit[rightNode.Left.Item] + 1;
+
+            f(rightNode.Item, middle, height + 1);
+        }
+
+
+        int ma = Math.Max(leftWidth, rightWidth);
+        max[height] = Math.Max(ma, ma);
+
+        int mi = 100000000;
+
+        if (leftWidth != -1)
+        {
+            mi = Math.Min(mi, leftWidth);
+        }
+
+        if (rightWidth != -1)
+        {
+            mi = Math.Min(mi, rightWidth);
+        }
+
+        min[height] = Math.Min(min[height], mi);
+    }
+
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        nodes = new Node[n + 1];
+
+        int[] cnt = new int[n + 2];
+
+        for (int i = 0; i < n; i++)
+        {
+            args = Console.ReadLine().Split();
+
+            if (int.Parse(args[1]) != -1)
+            {
+                cnt[int.Parse(args[1])]++;
+            }
+
+            if (int.Parse(args[2]) != -1)
+            {
+                cnt[int.Parse(args[2])]++;
+            }
+
+            var p = GetNode(int.Parse(args[0]));
+
+            var l = GetNode(int.Parse(args[1]));
+            var r = GetNode(int.Parse(args[2]));
+
+            p.Left = l;
+            p.Right = r;
+        }
+
+        visit = new int[n + 1];
+
+        int rootidx = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (cnt[i] == 0)
+            {
+                rootidx = i;
+            }
+        }
+
+        DFS(nodes[rootidx]);
+
+
+        min = new int[500];
+        max = new int[500];
+
+        for (int i = 0; i < 500; i++)
+        {
+            min[i] = 100000000;
+        }
+
+        f(rootidx, 0, 0);
+        int idx = 0;
+        int result = 0;
+        int reusltIdx = 0;
+        while (true)
+        {
+            if (min[idx] == 100000000 || max[idx] == 0)
+            {
+                break;
+            }
+
+            if (result < max[idx] - min[idx])
+            {
+                result = max[idx] - min[idx];
+                reusltIdx = idx;
+            }
+            idx++;
+        }
+
+        if (reusltIdx != 0) reusltIdx += 2;
+        else reusltIdx++;
+        result += 1;
+        Console.Write(reusltIdx + " " + result);
+    }
+}
--- BaekJoon/29_2250/Program.cs
+++ BaekJoon/29_2250/Program.cs
@@ -1,34 +1,69 @@
-﻿StreamReader rd = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+﻿using System.Globalization;
+using System.Text;
+
+StreamReader rd = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
 StreamWriter wr = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 int case_num = int.Parse(rd.ReadLine());
 
-List<int> column = new();
-column.Add(0);
+List<(int value, int row)> column = new();
+column.Add((0, 0));
 
 Dictionary<int, (int left, int right)> map = new();
 
 for (int i = 0; i < case_num; i++)
 {
-    int[] input = Array.ConvertAll(rd.ReadLine().Split(' '), int.Parse);
-    map.Add(input[0], (input[1], input[2]));
+    int[] input_01 = Array.ConvertAll(rd.ReadLine().Split(' '), int.Parse);
+    map.Add(input_01[0], (input_01[1], input_01[2]));
 }
 
-Inorder(1);
+int root = 0;
+for (int i = 1; i <= case_num; i++)
+{
+    for (int j = 1; j <= case_num; j++)
+    {
+        if (map[j].left == i || map[j].right == i)
+        {
+            break;
+        }
+        if (j == case_num)
+        {
+            root = i;
+        }
+    }
+}
+
+Inorder(root, 1);
+int a = 0;
+int b = 0;
+for (int i = 1; i <= column.Max(x => x.row); i++)
+{
+    List<int> result = new();
+    for (int j = 1; j <= case_num; j++)
+    {
+        if (column[j].row == i)
+        {
+            result.Add(j);
+        }
+    }
+    if (b < result.Max() + 1 - result.Min())
+    {
+        b = result.Max() + 1 - result.Min();
+        a = i;
+    }
+}
+wr.WriteLine("{0} {1}", a, b);
 wr.Close();
 
 
-void Inorder(int A)
+void Inorder(int A, int row)
 {
     if (map[A].left != -1)
     {
-        Inorder(map[A].left);
+        Inorder(map[A].left, row + 1);
     }
-    column.Add(A);
+    column.Add((A, row));
     if (map[A].right != -1)
     {
-        Inorder(map[A].right);
+        Inorder(map[A].right, row + 1);
     }
-
 }
-
-S
