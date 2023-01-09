839855f95447eb6fbde63ef0bb04dbc6d4f6570a
LOHHAMILTON <tttggg963@naver.com>
2023-01-09 17:38:42 +0900

    2023.1.9.

---
+++ Baekjoon_02/2_1715/Program.cs
@@ -0,0 +1,34 @@
+﻿StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+PriorityQueue<int, int> queue = new PriorityQueue<int, int>();
+int case_Num = int.Parse(sr.ReadLine());
+
+if(case_Num == 1)
+{
+    int input = int.Parse(sr.ReadLine());
+    sw.WriteLine(0);
+}
+
+else
+{
+    for (int i = 0; i < case_Num; i++)
+    {
+
+        int input = int.Parse(sr.ReadLine());
+        queue.Enqueue(input, input);
+    }
+    int result = 0;
+    while (queue.Count > 1)
+    {
+        int a = queue.Dequeue(); // 10 20 30
+        int b = queue.Dequeue();
+        result += a + b;
+        if (queue.Count == 0)
+        {
+            break;
+        }
+        queue.Enqueue(a + b, a + b);
+    }
+    sw.WriteLine(result);
+}
+sw.Close();
---
+++ Baekjoon_02/3_1766/Program.cs
@@ -0,0 +1,51 @@
+﻿StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+PriorityQueue<int, int> queue = new PriorityQueue<int, int>();
+int[] input = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+int[] indegree = new int[input[0] + 1];
+List<List<int>> list = new List<List<int>>();
+for (int i = 0; i <= input[0]; i++)
+{
+    list.Add(new List<int>());
+}
+
+for (int i = 0; i < input[1]; i++)
+{
+    int[] input01 = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+    list[input01[0]].Add(input01[1]);
+    indegree[input01[1]]++;
+}
+
+for (int i = 1; i <= input[0]; i++)
+{
+    if (indegree[i] == 0)
+    {
+        queue.Enqueue(i, i);
+    }
+}
+
+List<int> result = new();
+
+
+while(queue.Count > 0)
+{
+    int data = queue.Dequeue();
+    result.Add(data);
+    for(int i = 0; i< list[data].Count; i++)
+    {
+        indegree[list[data][i]]--;
+        if (indegree[list[data][i]] == 0)
+        {
+            queue.Enqueue(list[data][i], list[data][i]);
+        }
+    }
+}
+for(int i = 0; i < result.Count; i++)
+{
+    sw.Write(result[i]);
+    sw.Write(' ');
+}
+sw.WriteLine();
+sw.Close();
+
+
